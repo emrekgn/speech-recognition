1ef6015b4b4d38efd36a2a460a1e44418b80d874
emrekgn <emrekaganakkaya@gmail.com>
2017-05-11 17:20:39 +0300

    Add hw4 report

--- assignment-4/SR.py
+++ assignment-4/SR.py
@@ -1,24 +1,36 @@
 # -*- coding: utf-8 -*-
 
 import speech_recognition as sr
+import os
 
-KEYWORDS = [("one", 1), ("two", 1), ("three", 0.75), ("four", 1), ("five", 1), ("six", 1), ("seven", 0.75),
-            ("eight", 1), ("nine", 1), ("zero", 1)]
+KEYWORDS = [("one", 1.0), ("two", 1.0), ("three", 1.0), ("four", 1.0), ("five", 1.0), ("six", 1.0), ("seven", 1.0),
+            ("eight", 1.0), ("nine", 1.0), ("zero", 1.0)]
 
 
 def main():
-    # obtain audio from the microphone
     r = sr.Recognizer()
+    for f in os.listdir("./records"):
+        if f.endswith(".wav"):
+            audio_file = os.path.join("./records", f)
+            print("Reading file {0}...".format(audio_file))
+            with sr.AudioFile(audio_file) as source:
+                audio = r.record(source)  # read the entire audio file
+                print("Trying to recognize...")
+                try:
+                    print("Record says: \"" + r.recognize_sphinx(audio, show_all=False) + "\"\n")
+                except sr.UnknownValueError:
+                    print("Could not understand audio")
+                except sr.RequestError as e:
+                    print("Error; {0}".format(e))
+
+    # obtain audio from the microphone
     with sr.Microphone() as source:
         print("Running...")
         r.adjust_for_ambient_noise(source)
         print("Say something!")
         audio = r.listen(source)
-
-    # recognize speech using Sphinx
     print("Trying to recognize...")
     try:
-        #print("You said: \"" + r.recognize_sphinx(audio, keyword_entries=KEYWORDS, show_all=False) + "\"")
         print("You said: \"" + r.recognize_sphinx(audio, show_all=False) + "\"")
     except sr.UnknownValueError:
         print("Could not understand audio")
